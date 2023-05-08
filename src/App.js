b6d44cfccc78442c8ed093b864afb96d8cdf73b6
Krizzon <kris4an80@gmail.com>
2023-05-09 01:19:19 +0300

    gpt3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Footer,
+  Blog,
+  Features,
+  WhatGPT3,
+  Header,
+  Possibility,
+} from "./containers";
 
-function App() {
+import {
+  CTA,
+  Brand,
+  Navbar
+} from './components'
+
+import './App.css'
+import './index.css'
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="gradient__bg">
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/containers/possibility/Possibillity.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './possibility.css'
+
+const Possibillity = () => {
+  return (
+    <div>Possibillity</div>
+  )
+}
+
+export default Possibillity
